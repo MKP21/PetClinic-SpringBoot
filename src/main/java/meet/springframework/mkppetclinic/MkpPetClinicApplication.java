90fcbf98504ebf4f8f0c819a59f232a7832d35cf
MKP21 <meetknpatel2@gmail.com>
2020-04-09 02:17:31 -0400

    adding project

---
+++ src/main/java/meet/springframework/mkppetclinic/MkpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package meet.springframework.mkppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkpPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/meet/springframework/mkppetclinic/MkpPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package meet.springframework.mkppetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkpPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
