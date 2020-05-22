6253421e40a56b7302f8bd82661d1bf116d2594d
MKP21 <meetknpatel2@gmail.com>
2020-05-22 19:10:12 -0400

    adding POJO (basic model)

---
+++ src/main/java/meet/springframework/mkppetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package meet.springframework.mkppetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/meet/springframework/mkppetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package meet.springframework.mkppetclinic.model;
+
+public class Vet extends Person{
+
+}
