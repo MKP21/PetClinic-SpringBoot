ae4068928f6300bef91caa4e66d42edd115e511e
MKP21 <meetknpatel2@gmail.com>
2020-05-25 17:35:36 -0400

    refactored services to interface

--- pet-clinic-data/src/main/java/meet/springframework/mkppetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/meet/springframework/mkppetclinic/services/OwnerService.java
@@ -2,11 +2,6 @@ package meet.springframework.mkppetclinic.services;
 
 import meet.springframework.mkppetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner,Long> {
     Owner findByLastName();
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/meet/springframework/mkppetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/meet/springframework/mkppetclinic/services/PetService.java
@@ -1,11 +1,7 @@
 package meet.springframework.mkppetclinic.services;
+
 import meet.springframework.mkppetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long> {
 
 }
