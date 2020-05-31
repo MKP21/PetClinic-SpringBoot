91a1e9fcd2cae5c0a0b0cbbae7992ef3231106a1
MKP21 <meetknpatel2@gmail.com>
2020-05-31 14:08:44 -0400

    map based implementation of petType

---
+++ pet-clinic-data/src/main/java/meet/springframework/mkppetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package meet.springframework.mkppetclinic.services;
+
+import meet.springframework.mkppetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
