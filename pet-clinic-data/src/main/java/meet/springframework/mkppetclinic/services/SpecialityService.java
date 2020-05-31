af9d253ab3b30b2b550a079091eef3f874e98822
MKP21 <meetknpatel2@gmail.com>
2020-05-31 15:03:22 -0400

    adding SpecialityServiceMap

---
+++ pet-clinic-data/src/main/java/meet/springframework/mkppetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package meet.springframework.mkppetclinic.services;
+
+import meet.springframework.mkppetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long> {
+
+}
