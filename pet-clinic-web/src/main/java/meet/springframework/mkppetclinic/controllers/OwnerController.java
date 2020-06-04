85a4e3d75f0ab933bc9887d2c3d433741ecb6f28
MKP21 <meetknpatel2@gmail.com>
2020-06-03 22:45:45 -0400

    added "display owner"

--- pet-clinic-web/src/main/java/meet/springframework/mkppetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/meet/springframework/mkppetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package meet.springframework.mkppetclinic.controllers;
 import meet.springframework.mkppetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -24,5 +27,12 @@ public class OwnerController {
     @RequestMapping({"/find","/find.html"})
     public String find(Model model){
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
